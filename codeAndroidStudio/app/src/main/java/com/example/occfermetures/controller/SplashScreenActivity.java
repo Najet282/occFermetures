8140d015b134d14a8755eb1cfc86e78e736e7b8a
najet <82023253+Najet282@users.noreply.github.com>
2021-11-25 11:56:23 +0100

    amelioration plus ajout SplashScreenActivity

---
+++ codeAndroidStudio/app/src/main/java/com/example/occfermetures/controller/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.occfermetures.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.occfermetures.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //redirige vers la page principale MainActivity apres 3 secondes
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //demarrer une page
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        //creation d un handler post delayed
+        new Handler().postDelayed(runnable, 3000);
+
+    }
+}
